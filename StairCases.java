46bdc7a5da65a8cdddb449a56d9638c614ab5d3f
Lindsey Yu ッ <lyu2018@pingry.org>
2016-09-25 16:18:41 -0400

    first working version.

--- StairCases.java
+++ StairCases.java
@@ -1,33 +1,87 @@
-/**
- * Write javadoc for your class here (and remove this comment, or course)
- *
- */
+import java.util.Scanner; //used to get user input
+
+
 
 public class StairCases
 {
+
   /**
-   * Main method demonstrates the other wo methods by calling each of them.
-   * @param args Standard array of commandline String arguments. Not used in this program.
-   */
+ * This class represents a program that 
+ * gets an integer input from the user and prints 
+ * ascending and descending staircases of 'x' characters
+ * @author Lindsey Yu
+ * @version 0.1
+ */
+
 
   public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
+     
+     /**
+      * Main method demonstrates the other two methods by calling each of them.
+      * @param args Standard array of commandline String arguments. Not used in this program.
+      */
+    
+    Scanner keyboard = new Scanner(System.in); //set up for user input
+
+    System.out.println("Type in an integer: ");
+    int size = keyboard.nextInt();
 
     printAscending(size);
     printDescending(size);
   }
 
-  /**
-   * Write your javadoc here
-   */
-  public static void printAscending
-  //TODO Finish this method
 
-  /**
-   * Write your javadoc here
-   */
-  public static void printDescending
-  //TODO Finish this method
+  public static void printAscending(int size)
+
+  {  
+       /**
+       * This method takes in an integer (size) and prints the stars in an ascending fashion
+       * @param size the number of stars we want to build up to 
+       * no return statement 
+       */
+
+   
+    int count = 0;
+    
+    for (int i = 0; i < size; i++ ){
+      count = count + 1;
+
+      for(int n = 0; n < count; n++) {
+         System.out.print("x");
+      }
+
+      System.out.println(" ");
+    
+     }
+     System.out.println(" ");
+
+     
+  }
+
+
+  public static void printDescending(int size)
+
+  {
+
+     /**
+       * This method takes in an integer (size) and prints the xs in a descending fashion
+       * @param size the number of stars we want to count down from 
+       * no return statement 
+       */
+
+    int count = (size + 1);
+
+    for (int i = 0; i < size; i++) {
+      
+        count = count-1;
+      
+        for (int n = 0; n <count; n++) {
+          System.out.print("x");
+        }
+
+        System.out.println(" ");
+    }
+
+  }
 }
